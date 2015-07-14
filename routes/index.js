5e8cf65bde6829472baa3ebc45e98fa53c4bb6f0
aalarcond <sharkzizou@gmail.com>
2015-07-14 18:11:22 +0200

    Preparacion ejercicio Modulo 7

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,17 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// GET home page.
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
 //autoload
-router.param('quizID',quizController.load);
+router.param('quizId',quizController.load);
 
 //definicion de rutas /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-
-router.get('/author', function(req, res){
-	res.render('author', {errors: []});
-});
 
 module.exports = router;
